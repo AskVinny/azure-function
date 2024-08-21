95759299a88bea2dfbfb0a842ae4fd82365feaab
Rory Buchanan <rory@askvinny.co.uk>
2024-08-21 13:31:14 +0100

    testing

--- function_app.py
+++ function_app.py
@@ -26,3 +26,10 @@ def python_function_azure(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+    
+# Add this new simple function
+@app.route(route="hello", auth_level=func.AuthLevel.ANONYMOUS)
+def hello(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    name = req.params.get('name', "World")
+    return func.HttpResponse(f"Hello, {name}!")
